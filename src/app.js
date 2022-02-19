7a06b4aaf801b8084eeed20caf0804ce18f99973
Void24x <shadowwarrior591@gmail.com>
2022-02-19 17:57:32 +0530

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ const forecast = require('./utils/forecast')
 
 
 const app = express()
+const port =  process.env.PORT || 3000
 
 //define path for express config
 const publicPathDirectory = path.join(__dirname, '../public')
@@ -102,8 +103,8 @@ app.get('*' , (req,res) => {
     })
 })
 
-app.listen(3000 , () => {
-    console.log("Server is up at port 3000.")
+app.listen(port , () => {
+    console.log("Server is up at port "+ port)
 })
 
 
